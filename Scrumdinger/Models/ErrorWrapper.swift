d1c060872cae337c017f8958a1dc2373ed9f033d
Francis Dias <francisdiasbr@gmail.com>
2024-06-10 10:23:44 -0300

    Initial Commit

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Francis Dias on 08/06/24.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,46 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Francis Dias on 08/06/24.
+//
+
+import SwiftUI
+
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
